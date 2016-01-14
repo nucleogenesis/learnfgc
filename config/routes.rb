3db46af1ea883d82456f67ba746afb52344912e3
Jacob Pierce <w.jacobpierce@gmail.com>
2016-01-14 10:05:44 -0800

    Added basic styles for articles#show

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "/game", to: "articles#game_index"
   get "/game/character", to: "articles#character_index"
   get "/game/character/frame-data", to: "articles#character_frame_data"
+  get "/article", to: "articles#show"
 end
