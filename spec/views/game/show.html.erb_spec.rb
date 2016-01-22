e674963c01b7c35b196000c2f2c2c6eee06d932a
Jacob Pierce <w.jacobpierce@gmail.com>
2016-01-22 15:07:54 -0800

    Added a mite of customization to character#show

---
+++ spec/views/game/show.html.erb_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe "game/show.html.erb", type: :view do
+  describe "View Expectations" do
+    it "Lists All Characters & Their Appropriate Links"
+    it "Lists the 10 most recent articles related to the game"
+  end
+end
