77a287ed3ef9c9c65e4d5484a3d3bacaa42b7ade
Jacob Pierce <w.jacobpierce@gmail.com>
2016-01-22 12:15:02 -0800

    Removed revision model

--- db/migrate/20160122002244_create_revisions.rb
+++
@@ -1,8 +0,0 @@
-class CreateRevisions < ActiveRecord::Migration
-  def change
-    create_table :revisions do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
--- spec/factories/revisions.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :revision do
-    
-  end
-
-end
