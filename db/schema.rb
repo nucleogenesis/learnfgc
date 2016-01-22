7228c5ec26ec5bad3ba7ddc4b5d0eac4440ff560
Jacob Pierce <w.jacobpierce@gmail.com>
2016-01-22 09:41:30 -0800

    Added Tag/Tagging models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122171121) do
+ActiveRecord::Schema.define(version: 20160122174037) do
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at",   null: false
@@ -66,6 +66,24 @@ ActiveRecord::Schema.define(version: 20160122171121) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "article_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "taggings", ["article_id"], name: "index_taggings_on_article_id"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name"
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                     null: false
---
+++ spec/factories/taggings.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :tagging do
+    article nil
+tag nil
+  end
+
+end
