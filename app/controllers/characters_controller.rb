55a228bbd98e8715e2328fa2479ea45c775dabe6
Jacob Pierce <w.jacobpierce@gmail.com>
2016-01-28 08:40:50 -0800

    added character_controller specs for main page, others are tbd

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -2,7 +2,7 @@ class CharactersController < ApplicationController
   def show
     @character = Character.find_by_slug(params[:id])
     @game = @character.game
-    @articles = Article.order('created_at DESC').page((params[:page] || 1))
+    @articles = Article.where(character_id: @character.id).order('created_at DESC').page((params[:page] || 1))
   end
 
   def frame_data
--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -1,4 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe CharactersController, type: :controller do
+  let(:game) { create(:game) }
+  let(:character) { create(:character, game_id: game.id) }
+
+  describe "#show" do
+    it "assigns the character to @character" do
+      get :show, game_id: game.id, id: character.slug
+
+      expect(assigns(:character).name).to eq(character.name)
+      expect(assigns(:character).history).to eq(character.history)
+      expect(assigns(:character).slug).to eq(character.slug)
+    end
+    it "assigns the character's game to @game" do
+      get :show, game_id: game.id, id: character.slug
+
+      expect(assigns(:game).id).to eq(character.game_id)
+      expect(assigns(:game).title).to eq(character.game.title)
+    end
+    it "assigns a page of articles to @articles" do
+      other_character = create(:character, game_id: game.id)
+      user = build_stubbed(:user)
+      20.times { create(:random_article, user_id: user.id, game_id: game.id, character_id: character.id) }
+      20.times { create(:random_article, user_id: user.id, game_id: game.id, character_id: other_character.id) }
+
+      get :show, game_id: game.id, id: character.slug
+
+      expect(assigns(:articles).length).to eq(Article.default_per_page)
+      assigns(:articles).each do |a|
+        expect(a.character_id).to eq(character.id)
+      end
+
+      get :show, game_id: game.id, id: other_character.slug
+
+      expect(assigns(:articles).length).to eq(Article.default_per_page)
+      assigns(:articles).each do |a|
+        expect(a.character_id).to eq(other_character.id)
+      end
+    end
+  end
+  describe "#frame_data" do
+    it "assigns the character to @character"
+    it "assigns the character's frame data to @frame_data"
+  end
+  describe "#combos" do
+    it "assigns the character to @character"
+    it "assigns the character's combos to @combos"
+  end
 end
